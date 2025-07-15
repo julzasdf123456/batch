7cfa99fcd9c46fb1a86ecaf8b7c611478266ea8e
Esteban-ExpenseTracker <rei.nhard3367@gmail.com>
2025-07-15 13:14:53 +0800

    Cleaned Pages remove uneccessary functions and mountings for faster props loads

--- resources/js/components/students/view-student-classes-history.vue
+++ resources/js/components/students/view-student-classes-history.vue
@@ -189,19 +189,14 @@
 <script>
 import axios from "axios";
 import moment from "moment";
-import FlatPickr from "vue-flatpickr-component";
 import { Bootstrap4Pagination } from "laravel-vue-pagination";
-import "flatpickr/dist/flatpickr.css";
+
 import jquery from "jquery";
 import Swal from "sweetalert2";
-
-import FullCalendar from "@fullcalendar/vue3";
-import dayGridPlugin from "@fullcalendar/daygrid";
 
 export default {
   name: "ViewStudent.view-student",
   components: {
-    FlatPickr,
     Swal,
     pagination: Bootstrap4Pagination,
     jquery,
@@ -231,43 +226,10 @@ export default {
         showConfirmButton: false,
         timer: 3000,
       }),
-      pickerOptions: {
-        enableTime: false,
-        dateFormat: "Y-m-d",
-      },
-      studentData: {},
-      subjects: [],
-      payables: [],
 
-      activePayable: {},
-      paymentFor: "",
-      payableTransactionHistory: [],
-      tuitionsBreakdown: [],
-      isModalTuition: false,
-      allTransactions: [],
-      payableInclusions: [],
-      calendarOptions: {
-        plugins: [dayGridPlugin],
-        initialView: "dayGridMonth",
-        selectable: true,
-        height: 650,
-        width: 700,
-        eventOrderStrict: false,
-        themeSystem: "bootstrap",
-        headerToolbar: {
-          left: "prev,next today",
-          center: "title",
-          right: "dayGridMonth",
-        },
-        events: [],
-      },
-      attendanceData: [],
-      scholarships: [],
-      transactionDetails: [],
-      detailedTransactions: [],
+      studentData: {},
       selectedFile: null,
       imagePreview: null,
-      updateLogs: [],
       studentClassesHistoryData: [],
     };
   },
@@ -305,42 +267,7 @@ export default {
       // If none of the above, it's not null, empty, or undefined
       return false;
     },
-    validateNullStrings(string) {
-      return this.isNull(string) ? "" : string;
-    },
-    toMoney(value) {
-      if (this.isNumber(value)) {
-        return Number(parseFloat(value).toFixed(2)).toLocaleString("en-US", {
-          maximumFractionDigits: 2,
-          minimumFractionDigits: 2,
-        });
-      } else {
-        return "-";
-      }
-    },
-    isNumber(value) {
-      return typeof value === "number";
-    },
-    round(value) {
-      return Math.round((value + Number.EPSILON) * 100) / 100;
-    },
-    generateRandomString(length) {
-      const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-      let result = "";
 
-      for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        result += characters.charAt(randomIndex);
-      }
-
-      return result;
-    },
-    generateUniqueId() {
-      return moment().valueOf() + "-" + this.generateRandomString(32);
-    },
-    generateId() {
-      return moment().valueOf();
-    },
     getStudentDetails() {
       axios
         .get(`${this.baseURL}/students/get-student-details`, {
@@ -366,125 +293,7 @@ export default {
           });
         });
     },
-    getTotalBalance() {
-      var total = 0;
-      for (let i = 0; i < this.payables.length; i++) {
-        var balance = this.isNull(this.payables[i])
-          ? 0
-          : this.isNull(this.payables[i].Balance)
-          ? 0
-          : this.payables[i].Balance.length < 1
-          ? 0
-          : parseFloat(this.payables[i].Balance);
-        total += balance;
-      }
 
-      return total;
-    },
-    getActivePayable(id) {
-      this.activePayable = this.payables.find((obj) => obj.id === id);
-    },
-    transactionHistory(id) {
-      this.getActivePayable(id);
-      this.paymentFor = this.activePayable.PaymentFor;
-      this.isModalTuition = this.activePayable.Category === "Tuition Fees" ? true : false;
-      this.getTransactionHistory(id);
-
-      // init modal
-      let modalElement = this.$refs.modalTransactionHistory;
-      $(modalElement).modal("show");
-    },
-    getTransactionHistory(payableId) {
-      axios
-        .get(`${this.baseURL}/transactions/get-transactions-from-payable`, {
-          params: {
-            PayableId: payableId,
-          },
-        })
-        .then((response) => {
-          this.tuitionsBreakdown = response.data.TuitionLogs;
-          this.payableTransactionHistory = response.data.Transactions;
-          this.payableInclusions = response.data.PayableInclusions;
-          this.updateLogs = response.data.UpdateLogs;
-        })
-        .catch((error) => {
-          console.log(error);
-          this.toast.fire({
-            icon: "error",
-            text: "Error getting transaction history data!",
-          });
-        });
-    },
-
-    scholarshipWizzard() {
-      window.location.href =
-        this.baseURL +
-        "/student_scholarships/scholarship-wizzard/" +
-        this.studentId +
-        "/student-view";
-    },
-    getBarcodeAttendace() {
-      axios
-        .get(`${this.baseURL}/barcode_attendances/get-student-logs`, {
-          params: {
-            StudentId: this.studentId,
-          },
-        })
-        .then((response) => {
-          this.attendanceData = response.data;
-
-          var event = [];
-          for (let i = 0; i < this.attendanceData.length; i++) {
-            event.push({
-              title: this.attendanceData[i].PunchType,
-              start: moment(this.attendanceData[i].created_at).format(
-                "YYYY-MM-DD HH:mm:ss"
-              ),
-            });
-          }
-
-          this.calendarOptions.events = event;
-        })
-        .catch((error) => {
-          console.log(error);
-          this.toast.fire({
-            icon: "error",
-            text: "Error getting student data!",
-          });
-        });
-    },
-    removeScholarship(id) {
-      Swal.fire({
-        title: "Remove Scholarship?",
-        showCancelButton: true,
-        text: "You can always re-add a scholarship in the scholarship wizzard.",
-        confirmButtonText: "Proceed Removal",
-        confirmButtonColor: "#3a9971",
-      }).then((result) => {
-        if (result.isConfirmed) {
-          axios
-            .post(`${this.baseURL}/student_scholarships/remove-scholarship`, {
-              _token: this.token,
-              id: id,
-            })
-            .then((response) => {
-              this.toast.fire({
-                icon: "success",
-                text: "Scholarship removed!",
-              });
-              this.scholarships = this.scholarships.filter((obj) => obj.id !== id);
-              location.reload();
-            })
-            .catch((error) => {
-              console.log(error.response);
-              Swal.fire({
-                icon: "error",
-                text: error.response.data,
-              });
-            });
-        }
-      });
-    },
     deleteStudent() {
       Swal.fire({
         title: "Remove Student?",
@@ -518,9 +327,6 @@ export default {
             });
         }
       });
-    },
-    printTuitionLedger() {
-      window.location.href = `${this.baseURL}/transactions/print-tuition-ledger/${this.studentId}/${this.activePayable.SchoolYear}`;
     },
 
     // upload photo
@@ -598,8 +404,6 @@ export default {
   created() {},
   mounted() {
     this.getStudentDetails();
-
-    this.getBarcodeAttendace();
     this.getStudentClassesHistoryDetails();
   },
 };
